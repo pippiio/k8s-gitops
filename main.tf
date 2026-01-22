e40534bc7d623a801b867cb767c6c91b673e5405
Joe Sørensen <joe@techchapter.com>
2026-01-22 18:13:55 +0100

    feat: Add provider flux and install flux2

--- main.tf
+++ main.tf
@@ -4,3 +4,37 @@
  * Kubernetes GitOps module powered by FluxCD and ArgoCD
  *
  */
+
+provider "kubernetes" {
+  config_path    = "adeo-config.yaml"
+  config_context = "admin@adeo"
+}
+
+provider "flux" {
+  kubernetes = {
+    config_path    = "adeo-config.yaml"
+  }
+}
+
+resource "kubernetes_namespace_v1" "flux_system" {
+  metadata {
+    name = "flux-system"
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "adeo-config.yaml"
+  }
+}
+
+resource "helm_release" "flux2" {
+  repository = "https://fluxcd-community.github.io/helm-charts"
+  chart      = "flux2"
+  version    = "2.12.4"
+
+  name      = "flux2"
+  namespace = "flux-system"
+
+  depends_on = [kubernetes_namespace_v1.flux_system]
+}
--- versions.tf
+++ versions.tf
@@ -2,5 +2,9 @@ terraform {
   required_version = ">= 1.14"
 
   required_providers {
+    flux = {
+      source = "fluxcd/flux"
+      version = "1.7.6"
+    }
   }
 }
